0513c00be5aaff489569f6625771b2a60afd8bf5
rafiakbar07 <rafiakbarhakim04@gmail.com>
2025-01-10 15:00:11 +0700

    first commit

---
+++ gallery_foto.php
@@ -0,0 +1,160 @@
+<table class="table table-hover">
+    <thead class="table-dark">
+        <tr>
+            <th>No</th>
+            <th class="w-25">Judul</th>
+            <th class="w-75">Isi</th>
+            <th class="w-25">Gambar</th>
+            <th class="w-25">Aksi</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        include "koneksi.php";
+
+        // Paginasi
+        $hlm = (isset($_GET['hlm'])) ? intval($_GET['hlm']) : 1;
+        $limit = 3;
+        $limit_start = ($hlm - 1) * $limit;
+        $no = $limit_start + 1;
+
+        // Query dengan paginasi
+        $sql = "SELECT * FROM galery ORDER BY tanggal DESC LIMIT $limit_start, $limit";
+        $hasil = $conn->query($sql);
+
+        while ($row = $hasil->fetch_assoc()) {
+        ?>
+            <tr>
+                <td><?= $no++ ?></td>
+                <td>
+                    <strong><?= $row["judul"] ?></strong>
+                    <br>pada : <?= $row["tanggal"] ?>
+                </td>
+                <td><?= $row["isi"] ?></td>
+                <td>
+                    <?php
+                    if ($row["foto"] != '' && file_exists('img/' . $row["foto"])) {
+                        echo '<img src="img/' . $row["foto"] . '" width="100">';
+                    } else {
+                        echo 'Gambar tidak tersedia';
+                    }
+                    ?>
+                </td>
+                <td>
+                    <!-- Tombol aksi edit dan delete -->
+                    <a href="#" title="edit" class="badge rounded-pill text-bg-success" data-bs-toggle="modal" data-bs-target="#modalEdit<?= $row["id"] ?>"><i class="bi bi-pencil"></i></a>
+                    <a href="#" title="delete" class="badge rounded-pill text-bg-danger" data-bs-toggle="modal" data-bs-target="#modalHapus<?= $row["id"] ?>"><i class="bi bi-x-circle"></i></a>
+                    
+                    <!-- Modal Edit -->
+                    <div class="modal fade" id="modalEdit<?= $row["id"] ?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Edit Gallery</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <form method="post" action="update_galeri.php" enctype="multipart/form-data">
+                                    <div class="modal-body">
+                                        <div class="mb-3">
+                                            <label for="formGroupExampleInput" class="form-label">Judul</label>
+                                            <input type="hidden" name="id" value="<?= $row["id"] ?>">
+                                            <input type="text" class="form-control" name="judul" placeholder="Tuliskan Judul Artikel" value="<?= $row["judul"] ?>" required>
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="floatingTextarea2">Isi</label>
+                                            <textarea class="form-control" placeholder="Tuliskan Isi Artikel" name="isi" required><?= $row["isi"]?></textarea>
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="formGroupExampleInput2" class="form-label">Ganti Gambar</label>
+                                            <input type="file" class="form-control" name="gambar">
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="formGroupExampleInput3" class="form-label">Gambar Lama</label>
+                                            <?php
+                                            if ($row["foto"] != '' && file_exists('img/' . $row["foto"])) {
+                                                echo '<br><img src="img/' .$row["foto"] . '" width="100">';
+                                            }
+                                            ?>
+                                            <input type="hidden" name="gambar_lama" value="<?= $row["foto"] ?>">
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <input type="submit" value="Simpan" name="simpan" class="btn btn-primary">
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Modal Hapus -->
+                    <div class="modal fade" id="modalHapus<?= $row["id"] ?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Konfirmasi Hapus Artikel</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <form method="post" action="hapus_galeri.php">
+                                    <div class="modal-body">
+                                        <div class="mb-3">
+                                            <label for="formGroupExampleInput" class="form-label">Yakin akan menghapus artikel "<strong><?= $row["judul"] ?></strong>"?</label>
+                                            <input type="hidden" name="id" value="<?= $row["id"] ?>">
+                                            <input type="hidden" name="foto" value="<?= $row["foto"] ?>">
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                                        <input type="submit" value="Hapus" name="hapus" class="btn btn-primary">
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </td>
+            </tr>
+        <?php
+        }
+        ?>
+    </tbody>
+</table>
+
+<?php
+$sql = "SELECT * FROM galery";
+$hasil = $conn->query($sql);
+$jmldata = $hasil->num_rows;
+?>
+<p>Total gallery: <?php echo $jmldata; ?></p>
+<nav class="mb-2">
+    <ul class="pagination justify-content-end">
+        <?php
+        $jumlah_page = ceil($jmldata / $limit);
+        $jumlah_number = 1; // jumlah halaman ke kanan dan kiri dari halaman yang aktif
+        $start_number = ($hlm > $jumlah_number) ? $hlm - $jumlah_number : 1;
+        $end_number = ($hlm < ($jumlah_page - $jumlah_number)) ? $hlm + $jumlah_number : $jumlah_page;
+
+        if ($hlm == 1) {
+            echo '<li class="page-item disabled"><a class="page-link" href="#">First</a></li>';
+            echo '<li class="page-item disabled"><a class="page-link" href="#"><span aria-hidden="true">&laquo;</span></a></li>';
+        } else {
+            $link_prev = $hlm - 1;
+            echo '<li class="page-item"><a class="page-link" href="?hlm=1">First</a></li>';
+            echo '<li class="page-item"><a class="page-link" href="?hlm=' . $link_prev . '"><span aria-hidden="true">&laquo;</span></a></li>';
+        }
+
+        for ($i = $start_number; $i <= $end_number; $i++) {
+            $link_active = ($hlm == $i) ? ' active' : '';
+            echo '<li class="page-item' . $link_active . '"><a class="page-link" href="?hlm=' . $i . '">' . $i . '</a></li>';
+        }
+
+        if ($hlm == $jumlah_page) {
+            echo '<li class="page-item disabled"><a class="page-link" href="#"><span aria-hidden="true">&raquo;</span></a></li>';
+            echo '<li class="page-item disabled"><a class="page-link" href="#">Last</a></li>';
+        } else {
+            $link_next = $hlm + 1;
+            echo '<li class="page-item"><a class="page-link" href="?hlm=' . $link_next . '"><span aria-hidden="true">&raquo;</span></a></li>';
+            echo '<li class="page-item"><a class="page-link" href="?hlm=' . $jumlah_page . '">Last</a></li>';
+        }
+        ?>
+    </ul>
+</nav>
